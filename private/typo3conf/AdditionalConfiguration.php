4aaf10f49c1bc4c0f11f665c6b905ebb21d06193
Frank Berger <fberger@b-factor.de>
2019-12-09 17:03:46 +0100

    push to 10.2.x mit secure setup

--- private/typo3conf/AdditionalConfiguration.php
+++ private/typo3conf/AdditionalConfiguration.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$currentApplicationContext = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext();
+$currentApplicationContext = \TYPO3\CMS\Core\Core\Environment::getContext();
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] .= ' (' . (string)$currentApplicationContext . ')';
 
 if (!$currentApplicationContext->isProduction()) {
-    $contextConfigFile = dirname(PATH_site) . '/Configuration/' . (string)$currentApplicationContext . '.php';
+    $contextConfigFile = dirname(\TYPO3\CMS\Core\Core\Environment::getPublicPath()) . '/Configuration/' . (string)$currentApplicationContext . '.php';
     if (file_exists($contextConfigFile)) {
         require($contextConfigFile);
     }
