d43f343cad057368928aada7c95c1c4f67416bfd
Frank Berger <fberger@sudhaus7.de>
2021-10-01 17:49:43 +0200

    Version 11

--- private/typo3conf/AdditionalConfiguration.php
+++ private/typo3conf/AdditionalConfiguration.php
@@ -1,11 +1,13 @@
 <?php
 
 
-$currentApplicationContext = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext();
+use TYPO3\CMS\Core\Core\Environment;
+
+$currentApplicationContext                     = Environment::getContext();
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] .= ' (' . (string)$currentApplicationContext . ')';
 
 if (!$currentApplicationContext->isProduction()) {
-    $contextConfigFile = dirname(PATH_site) . '/Configuration/' . (string)$currentApplicationContext . '.php';
+    $contextConfigFile = Environment::getProjectPath(). '/Configuration/' . (string)$currentApplicationContext . '.php';
     if (file_exists($contextConfigFile)) {
         require($contextConfigFile);
     }
