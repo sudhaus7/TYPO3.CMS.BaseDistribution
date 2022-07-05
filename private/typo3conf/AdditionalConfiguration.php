61a9631301365853cc372fa377f1c325ef9b85f1
Frank Berger <fberger@sudhaus7.de>
2022-07-05 14:15:56 +0200

    added typo3 coding standards and pre-commit hook

--- private/typo3conf/AdditionalConfiguration.php
+++ private/typo3conf/AdditionalConfiguration.php
@@ -4,9 +4,23 @@
 $currentApplicationContext = \TYPO3\CMS\Core\Core\Environment::getContext();
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] .= ' (' . (string)$currentApplicationContext . ')';
 
-if (!$currentApplicationContext->isProduction()) {
-    $contextConfigFile = \TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/Configuration/' . (string)$currentApplicationContext . '.php';
-    if (file_exists($contextConfigFile)) {
-        require($contextConfigFile);
-    }
+/*
+We are loading an additional, optional configuration from INSTALLROOT/Configuration,
+based on the Application Context. Uf you need to set your own environment
+add a .php file in the appropriate directory in INSTALLROOT.
+For example if your TYPO3_CONTEXT variable is Development/fberger create a
+file INSTALLROOT/Configuration/Development/fberger.php
+
+You can add this to the git if needed. This way different developers can have
+different environments.
+
+There can be an INSTALLROOT/Configuration/Production.php file which contains
+additional configurations (managed in GIT for example) for production. If you
+need to add production specific configurations please add them there.
+*/
+
+$contextConfigFile = \TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/Configuration/' . (string)$currentApplicationContext . '.php';
+if (file_exists($contextConfigFile)) {
+    require($contextConfigFile);
 }
+
