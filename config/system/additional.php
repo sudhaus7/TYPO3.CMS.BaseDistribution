79822833a81ddd9402b872decd9b0bbaf1d81f36
Frank Berger <fberger@sudhaus7.de>
2023-02-07 11:47:15 +0100

    initial 12 version move dev configs to config/

--- config/system/additional.php
+++ config/system/additional.php
@@ -9,18 +9,20 @@ We are loading an additional, optional configuration from INSTALLROOT/Configurat
 based on the Application Context. Uf you need to set your own environment
 add a .php file in the appropriate directory in INSTALLROOT.
 For example if your TYPO3_CONTEXT variable is Development/fberger create a
-file INSTALLROOT/Configuration/Development/fberger.php
+file INSTALLROOT/config/development/fberger.php
 
 You can add this to the git if needed. This way different developers can have
 different environments.
 
-There can be an INSTALLROOT/Configuration/Production.php file which contains
+There can be an INSTALLROOT/config/production.php file which contains
 additional configurations (managed in GIT for example) for production. If you
 need to add production specific configurations please add them there.
 */
 
-$contextConfigFile = \TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/Configuration/' . (string)$currentApplicationContext . '.php';
-if (file_exists($contextConfigFile)) {
-    require($contextConfigFile);
+
+if (file_exists( \TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/config/' . (string)strtolower( $currentApplicationContext) . '.php')) {
+    require(\TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/config/' . (string)strtolower( $currentApplicationContext) . '.php');
+} else if (file_exists( \TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/config/' . (string) $currentApplicationContext . '.php')) {
+    require(\TYPO3\CMS\Core\Core\Environment::getProjectPath(). '/config/' . (string)$currentApplicationContext . '.php');
 }
 
