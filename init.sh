6b486cbfbe7299af8e439292af591aeb9376decb
Frank Berger <fberger@sudhaus7.de>
2023-04-07 19:35:59 +0100

    added init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,13 @@ then
     $GIT init;
     $GIT add .
     $GIT config --local include.path ../.gitconfig
-    echo 'GIT is set up, do not forget to add your origin (the gitlab url for this project) with git origin add git@git.b-factor.de:customer/project or something similar'
+    echo "GIT is set up"
+    echo "Do not forget to add your origin (the gitlab url for this project)"
+    echo "with git origin add git@git.b-factor.de:customer/project or something similar"
+    echo
     echo 'You can now execute `composer guidedinstall` or import a typo3 database and execute `composer typo3install`'
+fi
+if [[ -x $GIT ]];
+then
+    $GIT config --local include.path ../.gitconfig
 fi
